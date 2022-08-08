956fd8ea0ca377b5da2b04fb0941578975095d28
thanhty67 <th.ty6701@gmail.com>
2022-08-08 17:00:36 +0700

    First commit

---
+++ AssGroup_OJT-Management-System/Solution1/Library/Repository/IRepositoryTblSemester.cs
@@ -0,0 +1,20 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Repository
+{
+    public interface IRepositoryTblSemester
+    {
+        public TblSemester GetCurrentSemester();
+        public void AddNewSemester(TblSemester newSemester);
+        public IEnumerable<string> GetAllSemesterName();
+        public TblSemester GetSemterBySemesterID(int id);
+
+        //TblSemester GetCurrentSemester();
+        public TblSemester GetSemesterByName(string name);
+    }
+}
